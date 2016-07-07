ece71c36a2c87ba331526ea8ca2551dd0f608fa0
fifiteen82726 <fifiteen82726@gmail.com>
2016-07-07 18:40:04 +0800

    add activeadmin, it can loggin, can't go dashboard or others

--- Gemfile
+++ Gemfile
@@ -37,6 +37,8 @@ gem 'pry-rails'
 
 gem 'devise'  
 gem 'cancancan'  
+
+gem 'activeadmin'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,11 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+AdminUser.destroy_all
 Post.destroy_all
 
 100.times do |i| 
 Post.create(title: Faker::Name.title, content: Faker::Hipster.paragraph)
 	end
+AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+Author.create(email: "123@gmail.com", password: '2wsx1qaz', password_confirmation: '2wsx1qaz')
